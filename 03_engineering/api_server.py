d541dea2b275328add45d6122be83a724ef4eb45
liliangnike <liliangnike@126.com>
2026-06-06 12:29:26 +0000

    Add windows edge browser testing steps

--- 03_engineering/api_server.py
+++ 03_engineering/api_server.py
@@ -3,6 +3,13 @@
 # 3. Open another terminal
 # 4. curl -X POST "http://127.0.0.1:8000/chat?user_prompt=What%20is%20Project-X-99?"
 
+# **** For Windows Edge 
+# 1. Open Windows cmd, execute "ssh -L 8000:127.0.0.1:8000 username@linux_server_ip"
+# 2. Enter into this project, execute "uvicorn api_server:app --host 0.0.0.0 --port 8000 --reload"
+# 3. Open Microsoft Edge, input "http://127.0.0.1:8000/docs"
+# 4. Click 'chat' and then 'try it out'
+# ****
+
 from fastapi import FastAPI
 from rag_agent import RAGAgent
 
